c438ae073bf71bd98b4259eec353518cf8d90a00
your-worst-taco <stephen@tafoya.dev>
2021-02-26 22:35:27 -0700

    Updated cleanup to use env

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ dotenv.config();
 
 let port = process.env.TACOCART_PORT || 3000;
 let host = process.env.TACOCART_HOST || 'localhost:3000';
+let minutes = process.env.TACOCART_MINUTES || 30;
+let max_age = process.env.TACOCART_MAX_AGE || 7;
+let max_age_once = process.env.TACOCART_MAX_AGE_ONCE || 1;
+let check_interval = minutes * 60 * 1000
 
 let standard_storage = path.join(__dirname, "tacos");
 if (!fs.existsSync(standard_storage)) {
@@ -20,16 +24,14 @@ if (!fs.existsSync(once_storage)) {
     fs.mkdirSync(once_storage);
 }
 
-let minutes = 0.1, check_interval = minutes * 60 * 1000
-
-async function file_cleanup(folder) {
+async function file_cleanup(folder, age) {
     const files = await fs.promises.readdir(folder);
     for (const file of files) {
 
         const info = await fs.promises.stat(path.join(folder, file))
         if (info.isFile()) {
             let check_date = new Date();
-            check_date.setDate(check_date.getDate() - 1);
+            check_date.setDate(check_date.getDate() - age);
 
             if (info.mtime < check_date) {
                 fs.rmSync(path.join(folder, file));
@@ -41,8 +43,8 @@ async function file_cleanup(folder) {
 
 setInterval(async () => {
     try {
-        await file_cleanup(standard_storage);
-        await file_cleanup(once_storage);
+        await file_cleanup(standard_storage, max_age);
+        await file_cleanup(once_storage, max_age_once);
     } catch (e) {
         console.log("Error:", e);
     }
