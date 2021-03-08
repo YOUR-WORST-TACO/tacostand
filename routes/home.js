49c686bae67a27cce2a01551cb54fdd5ce3fd0a0
your-worst-taco <stephen@tafoya.dev>
2021-03-08 00:56:45 -0700

    updated index.ejs, made it pretty

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,11 @@
 const Router = require('@koa/router');
 const config = require('../config');
+const host = require('../resources/hostname');
+
 const router = new Router();
 
 router.get('/', async(ctx, next) => {
-    await ctx.render('index', {config: config});
+    await ctx.render('index', {config: config, host: host()});
 })
 
 module.exports = router;
