8f8ca84e8666db2b9c80e4863a486144e05b7098
your-worst-taco <stephen@tafoya.dev>
2021-03-08 01:09:20 -0700

    fixed minor bug

--- resources/hostname.js
+++ resources/hostname.js
@@ -1,7 +1,7 @@
 const config = require('../config');
 
 module.exports = () => {
-    let port = config.server.port !== 443 || config.server.port !== 80 ? ":" + config.server.port : "";
+    let port = config.server.port !== '443' || config.server.port !== '80' ? ":" + config.server.port : "";
     let prefix = config.server.secure ? "https://" : "http://";
     return prefix + config.server.host + port + "/";
 }
